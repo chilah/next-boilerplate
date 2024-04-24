90a98f806b883ab26d46fb918347426568dff02d
Jirapong Kanha <jz.kanha@gmail.com>
2024-04-24 17:00:16 +0700

    feat: add check label

--- components/pattern/input/TextInput.tsx
+++ components/pattern/input/TextInput.tsx
@@ -3,24 +3,24 @@ import { BaseInput, Typography } from '@/components/common';
 import { BaseComponentProps } from '@/type';
 
 type TextInputProps = {
-  value: string;
-  onChange: (value: string) => void;
-  label?: string | React.ReactNode;
+    value: string;
+    onChange: (value: string) => void;
+    label?: string | React.ReactNode;
 } & BaseComponentProps;
 
 const TextInput: React.FC<TextInputProps> = ({ value, onChange, label }) => {
-  return (
-    <div className="flex flex-col gap-4">
-      <Typography>{label}</Typography>
-      <BaseInput
-        type="text"
-        value={value}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          onChange(e.target.value);
-        }}
-      />
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-4">
+            {label ? <Typography>{label}</Typography> : null}
+            <BaseInput
+                type="text"
+                value={value}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    onChange(e.target.value);
+                }}
+            />
+        </div>
+    );
 };
 
 export default TextInput;
